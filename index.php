d6ba46f2e1e1186a42c33da8a07504841b56e4d0
Myrna Willamsclear <2075740@talnet.nl>
2020-09-02 19:05:30 +0200

    updated

---
+++ index.php
@@ -0,0 +1,27 @@
+//Var
+<?php
+$getal=[8];
+ ?>
+
+//OEF 1
+<?php
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+echo var_dump($klas);
+?>
+
+//OEF 2
+<?php
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+for ($i=0; $i <1; $i++){
+  echo implode("\n", $klas);
+}
+?>
+
+//OEF 3
+<?php
+$bucket_list = [];
+$hoeveelheid_activiteiten = [];
+for ($i=0; $i <1; $i++){
+  echo implode("\n", $hoeveelheid_activiteiten);
+}
+?>
