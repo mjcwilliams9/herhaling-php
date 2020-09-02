7affe5d6589237659b91f9a5f53c4954b4e70dcc
Myrna Willamsclear <2075740@talnet.nl>
2020-09-02 20:59:29 +0200

    update

--- index.php
+++ index.php
@@ -12,16 +12,30 @@ echo var_dump($klas);
 //OEF 2
 <?php
 $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
-for ($i=0; $i <1; $i++){
-  echo implode("\n", $klas);
+for ($i=0; $i <4; $i++){
+  echo ($klas)[$i],PHP_EOL;
 }
 ?>
 
+
 //OEF 3
 <?php
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+foreach ($klas as $i) {
+  echo $i, PHP_EOL;
+}
+?>
+
+//COMBI
+<?php
 $bucket_list = [];
-$hoeveelheid_activiteiten = [];
-for ($i=0; $i <1; $i++){
-  echo implode("\n", $hoeveelheid_activiteiten);
+$hoeveelheid_activiteiten = readline("hoeveel activiteiten wil je toevoegen aan je bucket list?");
+for ($i=0; $i < $hoeveelheid_activiteiten ; $i++) {
+  $activiteit = readline("Welke activiteiten wil je toevoegen? ");
+}
+
+var_dump($bucket_list);
+foreach ($bucket_list as $b) {
+  echo $b, PHP_EOL;
 }
 ?>
